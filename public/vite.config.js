3cd601d6aecb6954e46e594fa1e44e4343f62c4d
Maxik230777 <maxik230777@yandex.ru>
2025-05-13 01:05:48 +0300

    root: 'public'

--- public/vite.config.js
+++ public/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // root: 'myshopapp/public',
+   root: 'public',
   plugins: [
     vue(),
     vueDevTools(),
